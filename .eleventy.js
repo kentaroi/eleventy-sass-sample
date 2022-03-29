aec3a4935b1c4efe973599ac696add042e0af37e
Kentaro Imai <kentaroi@gmail.com>
2022-03-29 17:17:34 +0900

    Add minimal code

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const eleventySass = require("eleventy-sass");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass, {
+    compileOptions: {
+      permalink: function(contents, inputPath) {
+        return (data) => {
+          return data.page.filePathStem.replace(/^\/scss\//, "/css/") + ".css";
+        };
+      }
+    }
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
+
